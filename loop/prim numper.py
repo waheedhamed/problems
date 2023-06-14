680f6289a4020c7b053a6586f954adfa65b9178c
waheedhamed <122164129+waheedhamed@users.noreply.github.com>
2023-06-14 14:31:31 +0300

    Create prim numper.py

---
+++ loop/prim numper.py
@@ -0,0 +1,14 @@
+def isprime(x) :
+ for i in range(2,a) :
+      if a%i ==0:
+        return False
+ return True 
+      
+     
+    
+    
+a=int(input())
+if isprime(a)==True :
+            print("prime")
+else :
+            print("not prime ") 
